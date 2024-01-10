c2d4f449750fa1bfb3cf59088bf742be29a9bdf6
Paulusz Zsombor <paulusz.zsombor@gmail.com>
2024-01-10 08:38:40 +0100

    teszt3

--- src/test/java/HexadecagonTest.java
+++ src/test/java/HexadecagonTest.java
@@ -1,5 +1,18 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import lan.zold.Hexadecagon;
 
 public class HexadecagonTest {
     Hexadecagon hexadecagon;
+
+    @BeforeTest
+    public void testCalc() {
+        this.hexadecagon = new Hexadecagon();
+    }
+
+    @Test
+    public void testCalc2() {
+        this.hexadecagon.calc();
+    }
 }
